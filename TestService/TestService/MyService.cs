63508810b1d54bc0a27ff48f8f9431e9e19c7a21
Pritesh1207 <pks.pritesh01@gmail.com>
2020-05-10 03:14:59 +0530

    Pritesh Changes 10/05/2020

---
+++ TestService/TestService/MyService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestService
+{
+    class MyService : IMyService
+    {
+     public string Getdata()
+      {
+      return "PRITESHKUMAR";
+      }
+    }
+}
